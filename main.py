089d056b9029e4088d9785352b3ec2ca37009164
Miró Sorja <miro.sorja@digital-identity.com>
2014-12-21 14:24:07 +0200

    inital

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Project: A*
+We'll see how it goes from there!
+Probably the only file used in the project.
+"""
+import sys
+import time
+def main():
+	#No args for now
+	print time.time();
+
+if __name__ == "__main__":
+    main()
