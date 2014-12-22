36873fab9b71f16a4d0e00a73651499fd5b5c166
Markus Pahkamaa <markus.pahkamaa@helsinki.fi>
2014-12-22 18:38:44 +0200

    luku + kokonaisluvun luonti.
    Luetaan annettu tiedosto.
    Luetusta tiedosta muodostetaan kokonaisluku (käyttää tällä hetkellä
    vielä longia).

--- Encryption/src/encryption/Encryption.java
+++ Encryption/src/encryption/Encryption.java
@@ -6,12 +6,54 @@
 
 package encryption;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.Reader;
+
 /**
  *
  * @author Markus
  */
 public class Encryption {
 
+    private static String input = new String();
+    
+    private static void generateInt(){
+        if ( !input.isEmpty() ){
+            long kokonaisluku = 0L;
+            
+            
+            int i = 0;
+            
+            while ( i < input.length() ){
+                char ch = input.charAt(i);
+                kokonaisluku += (long)ch*Math.pow(256, i);
+                i++;
+            }
+            System.out.println("Kokonaisluku: " + kokonaisluku);
+            
+        }
+        System.out.println("No data to generate integer");
+    }
+    
+    private static void readContents(File fileToRead) throws Exception{
+        if ( fileToRead.canRead()) {
+            FileReader fr = new FileReader(fileToRead);
+            BufferedReader bfr = new BufferedReader(fr);
+            String tmp = bfr.readLine();
+            while ( tmp != null ){
+                input = input + tmp;
+                tmp = bfr.readLine();
+            }
+            System.out.println("Tiedoston sisältö: " + input);
+        }
+        else{
+            throw new Exception("File cannot be read!");
+        }
+        
+    }
     /**
      * @param args the command line arguments
      */
@@ -35,6 +77,14 @@ public class Encryption {
         if ( args.length == 3 ){
             if ( args[0].equals("-encrypt")){
                 System.out.println("Ecrypting...");
+                File file = new File(args[2]);
+                try {
+                    readContents(file);
+                    generateInt();
+                }catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+                
             }
 
             if ( args[0].equals("-decrypt")){
