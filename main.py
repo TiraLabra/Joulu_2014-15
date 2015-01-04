e7523a05d5318714aa8224777cafde003aa72293
Miró Sorja <miro.sorja@gmail.com>
2015-01-04 20:44:08 +0200

    Added new method, doesn't work yet, fixed documentation, fixed minor/major problems, general commit

--- main.py
+++ main.py
@@ -8,18 +8,22 @@
 	1: wall, can't go here
 	2: start
 	3: end
+
+	Currently only prints the map and neighbours of 
+	chosen block.
+	Also how long it took to get neighbours and print map in millis.
 """
 import sys
 import time
 import random
-
+import Queue
 # 
 # Before allowing user to give input,
 # use some default hardcoded values
 # 
 
-STARTBLOCK = [1,1,2]
-ENDBLOCK = [4,4,3] 
+STARTBLOCK = 1,1
+ENDBLOCK = 4,4
 WIDTH = 10
 HEIGHT = 10
 """
@@ -30,7 +34,9 @@ def main():
 	start = time.time()
 	graph = getGraph(WIDTH,HEIGHT)
 	printMap(graph)
-	print neighbors([2,3], graph)
+	# This does not work yet:
+	# print mapTravel([1,1], graph)
+	print neighbors(STARTBLOCK, graph)
 	print time.time() - start
 """
 	Helper function to print the map correctly
@@ -38,15 +44,20 @@ def main():
 """
 def printMap(graph):
 	line = "1234567890"
-	for y in graph:
-		# print y
-		if(y[0] % WIDTH == 0): 
-			print(line + "        " + str(y[1]))
-			line = ""
-		if(y[2] == 0): line += "_"
-		if(y[2] == 1): line += "\033[91m" + "#" +"\033[0m"
-		if(y[2] == 2): line += "\033[92m" + "O" + "\033[0m"
-		if(y[2] == 3): line += "\033[92m" + "X" + "\033[0m"
+	def asd(line, row):
+		print line, "        ", row
+
+	for x,y,type_ in graph:
+		if x % WIDTH == 0: 
+			asd(line, y)
+			line = ''
+		if (x,y) == STARTBLOCK: line += "\033[92m" + "O" + "\033[0m"
+		elif (x,y) == ENDBLOCK: line += "\033[92m" + "X" + "\033[0m"
+		elif type_ == 0: line += "_"
+		elif type_ == 1: line += "\033[91m" + "#" +"\033[0m"
+
+	if line:
+		asd(line, y+1)
 
 """
 	Generates a graph with given width and height
@@ -54,21 +65,10 @@ def printMap(graph):
 	@w: Width of the graph
 """
 def getGraph(w,h):
-	nodes = []
-	for y in range(h+1):
-		for x in range(w):
-			if(x == 0 or x == w-1 or y == 0 or y == h-1):
-				nodes.append([x,y, 1])
-			elif(STARTBLOCK[0] == x and STARTBLOCK[1] == y):
-				nodes.append(STARTBLOCK)
-			elif(ENDBLOCK[0] == x+1 and ENDBLOCK[1] == y+1):
-				nodes.append(ENDBLOCK)
-			else:
-				if(random.random() > 0.8):
-					nodes.append([x,y,1])
-				else:
-					nodes.append([x,y,0])
-	return nodes
+	return [
+	(x, y, 1 if random.random() > 0.8 or x in (0,h-1) or y in (0,w-1) else 0)
+	for y in range(w) for x in range(h)
+	]
 
 """
 	Get neighbors of a node.
@@ -76,7 +76,7 @@ def getGraph(w,h):
 	@node: the node we want to find the neighbors for
 """
 def neighbors(node, graph):
-	#				right 	down	left		up
+	#			   right  down   left	  up
 	directions = [[1,0], [0,1], [-1, 0], [0, -1]]
 	nodes = []
 	for dir in directions:
@@ -87,12 +87,28 @@ def neighbors(node, graph):
 		for x in range(4):
 			curr = n[:]
 			curr.append(x)
-			print (str(curr) + str(curr in graph))
-			if curr in graph:
+			if tuple(curr) in graph:
 				nodes.append(curr)
 				break
 	return nodes
 
+"""
+	Travel the whole map using a queue
+"""
+def mapTravel(start, graph):
+	frontier = Queue.Queue()
+	# Start block:
+	frontier.put(start);
+	visited = {}
+	visited[tuple(start)] = True
+
+	#Loop as long as we have something to go thru
+	while not frontier.empty():
+		current = frontier.get()
+		for next in neighbors(current, graph):
+			if next not in tuple(visited):
+				frontier.put(next)
+				visited[tuple(next)] = True
 
 if __name__ == "__main__":
     main()
